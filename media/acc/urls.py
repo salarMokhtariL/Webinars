b196ea4ffb427b0c67d537191a8e9914eaceb538
salarMokhtariL <salarmokhtari0@gmail.com>
2022-05-12 18:01:35 +0430

    Update Navbar

--- media/acc/urls.py
+++ media/acc/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
+
+
 urlpatterns = [
-    path('Signin/', views.SignIn_views, name="Signin")
+    path('', views.Index, name="Index"),
+    path('Signin/', views.SignIn_views, name="Signin"),
+    path('message/', views.Message_views, name="message"),
+
 ]
--- media/acc/views.py
+++ media/acc/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+def Index(request):
+    contex ={}
+    return render(request, 'index.html', contex)
+    
+
 def SignIn_views(request):
     contex = {}
     return render(request, 'signUp.html', contex)
+
+
+def Message_views(request):
+    contex = {}
+    return render(request, 'message.html', contex)
